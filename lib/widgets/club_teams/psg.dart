df9b059f5b98698987d8e828180618ce521b3b12
najmulmyself <najmul.myself@gmail.com>
2022-03-27 01:45:53 +0600

    fixed with expanded and a column with fixed containeer height(300)

--- lib/widgets/club_teams/psg.dart
+++ lib/widgets/club_teams/psg.dart
@@ -9,54 +9,83 @@ class Psg extends StatelessWidget {
       appBar: AppBar(
         title: Text('PSG'),
       ),
-      body:
-          // Column(
-          //   children: [
-          //     Row(
-          //       children: [
-          //         Padding(
-          //           padding: const EdgeInsets.all(8.0),
-          //           child: Image.asset(
-          //             'assets/clubTeams/psg.png',
-          //             width: 100,
-          //             fit: BoxFit.contain,
-          //             height: 100,
-          //           ),
-          //         ),
-          //         Image.asset(
-          //           'assets/clubTeams/psg.png',
-          //           fit: BoxFit.contain,
-          //           width: 100,
-          //           height: 100,
-          //         ),
-          //       ],
-          //     ),
-          //   ],
-          // ),
-          Container(
-        margin: EdgeInsets.only(top: 5, right: 20, left: 20),
-        width: double.infinity,
-        height: double.infinity,
-        child: Row(
+      body: SingleChildScrollView(
+        child: Column(
           children: [
-            Expanded(
-              flex: 1,
-              child: Image(
-                image: AssetImage('assets/clubTeams/psg.png'),
+            Container(
+              margin: EdgeInsets.only(top: 5, right: 20, left: 20),
+              width: double.infinity,
+              height: 300,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Image(
+                      image: AssetImage('assets/clubTeams/psg.png'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Image(
+                      image: AssetImage('assets/clubTeams/psg.png'),
+                    ),
+                  ),
+                ],
               ),
             ),
-            SizedBox(
-              width: 10,
+            Container(
+              margin: EdgeInsets.only(top: 5, right: 20, left: 20),
+              width: double.infinity,
+              height: 300,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Image(
+                      image: AssetImage('assets/clubTeams/psg.png'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Image(
+                      image: AssetImage('assets/clubTeams/psg.png'),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Expanded(
-              flex: 1,
-              child: Image(
-                image: AssetImage('assets/clubTeams/psg.png'),
+            Container(
+              margin: EdgeInsets.only(top: 5, right: 20, left: 20),
+              width: double.infinity,
+              height: 300,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Image(
+                      image: AssetImage('assets/clubTeams/psg.png'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Image(
+                      image: AssetImage('assets/clubTeams/psg.png'),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
         ),
-        
       ),
     );
   }
