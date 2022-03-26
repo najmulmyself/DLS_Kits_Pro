ea3f0e1de52cea512468d702058e5566a4598182
najmulmyself <najmul.myself@gmail.com>
2022-03-27 01:41:40 +0600

    partially managed with expanded

--- lib/widgets/club_teams/psg.dart
+++ lib/widgets/club_teams/psg.dart
@@ -9,28 +9,54 @@ class Psg extends StatelessWidget {
       appBar: AppBar(
         title: Text('PSG'),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  'assets/clubTeams/psg.png',
-                  width: 100,
-                  fit: BoxFit.contain,
-                  height: 100,
-                ),
+      body:
+          // Column(
+          //   children: [
+          //     Row(
+          //       children: [
+          //         Padding(
+          //           padding: const EdgeInsets.all(8.0),
+          //           child: Image.asset(
+          //             'assets/clubTeams/psg.png',
+          //             width: 100,
+          //             fit: BoxFit.contain,
+          //             height: 100,
+          //           ),
+          //         ),
+          //         Image.asset(
+          //           'assets/clubTeams/psg.png',
+          //           fit: BoxFit.contain,
+          //           width: 100,
+          //           height: 100,
+          //         ),
+          //       ],
+          //     ),
+          //   ],
+          // ),
+          Container(
+        margin: EdgeInsets.only(top: 5, right: 20, left: 20),
+        width: double.infinity,
+        height: double.infinity,
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Image(
+                image: AssetImage('assets/clubTeams/psg.png'),
               ),
-              Image.asset(
-                'assets/clubTeams/psg.png',
-                fit: BoxFit.contain,
-                width: 100,
-                height: 100,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              flex: 1,
+              child: Image(
+                image: AssetImage('assets/clubTeams/psg.png'),
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
+        
       ),
     );
   }
