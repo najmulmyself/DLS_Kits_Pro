07d940b43643d1d3b095d2d53da8218241c835ad
najmulmyself <najmul.myself@gmail.com>
2022-06-23 22:52:11 +0600

    working on premier league

--- lib/widgets/club_teams/premier_league.dart
+++ lib/widgets/club_teams/premier_league.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../kit_page.dart';
+
 class PremierLeague extends StatelessWidget {
   const PremierLeague({ Key? key }) : super(key: key);
 
@@ -17,11 +19,38 @@ class PremierLeague extends StatelessWidget {
               width: double.infinity,
               height: 300,
               child: Row(
-                children: const [
+                children:  [
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/chelsea21.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              title: "CHELSEA",
+                              logo:
+                                  "https://i.imgur.com/fLQ27h0.png",
+                              centeredText: "CHELSEA KIT 21/22",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.ibb.co/NCswXbj/Pics-Art-04-01-05-07-56.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.ibb.co/qgQz7tV/p2-1.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.ibb.co/qsxZC59/p3.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "https://i.ibb.co/BjHxnVM/p4.png",
+                              gkaway: "Gk Away",
+                              gkawayI: "loading.....",
+                              gkThird: "Gk Third",
+                              gkThirdI: "loading.....",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/chelsea21.png'),
+                      ),
                     ),
                   ),
                   SizedBox(
