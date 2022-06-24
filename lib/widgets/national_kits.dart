901f9f311da74db36c095f2aaf252c43cf79cf5f
najmulmyself <najmul.myself@gmail.com>
2022-06-24 21:03:28 +0600

    added national kits

--- lib/widgets/national_kits.dart
+++ lib/widgets/national_kits.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'kit_page.dart';
+
 class NationalKits extends StatelessWidget {
   const NationalKits({Key? key}) : super(key: key);
 
@@ -16,27 +18,155 @@ class NationalKits extends StatelessWidget {
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
-              child: Image.asset('assets/images/N1.png'),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (ctx) => KitPage(
+                        logo: "https://i.imgur.com/r8BATuG.png",
+                        homeKit: "Home Kit",
+                        homeKitI: "https://i.imgur.com/l3Aejuo.png",
+                        awayKit: "Away Kit",
+                        awayKitI: "https://i.imgur.com/Z54OvQq.png",
+                        thirdKit: "Third Kit",
+                        thirdKitI: "https://i.imgur.com/Q6SBx77.png",
+                        gkHome: "Gk Home",
+                        gkHomeI: "https://i.imgur.com/u5tRp6L.png",
+                        gkaway: "Gk Away Kit",
+                        gkawayI: "https://i.imgur.com/fEC3J6T.png",
+                        gkThird: "Gk Third Kit",
+                        gkThirdI: "https://i.imgur.com/YRPfF8e.png",
+                        title: "BANGLADESH",
+                        centeredText: "BANGLADESH Kit",
+                      ),
+                    ),
+                  );
+                },
+                child: Image.asset(
+                  'assets/images/bangladesh.png',
+                ),
+              ),
             ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
-              child: Image.asset('assets/images/N2.png'),
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (ctx) => KitPage(
+                          logo: "https://i.imgur.com/8Pns3fH.png",
+                          homeKit: "Home Kit",
+                          homeKitI: "https://i.imgur.com/fJ0wl3S.png",
+                          awayKit: "Away Kit",
+                          awayKitI: "https://i.imgur.com/9q07Kcf.png",
+                          thirdKit: "Third Kit",
+                          thirdKitI: "loading....",
+                          gkHome: "Gk Home",
+                          gkHomeI: "https://i.imgur.com/ox1fU2w.png",
+                          gkaway: "Gk Away Kit",
+                          gkawayI: "https://i.imgur.com/rzqPXJs.png",
+                          gkThird: "Gk Third Kit",
+                          gkThirdI: "LOADING .....",
+                          title: "GERMANY",
+                          centeredText: "GERMANY Kit",
+                        ),
+                      ),
+                    );
+                  },
+                  child: Image.asset('assets/images/germany.png')),
             ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
-              child: Image.asset('assets/images/N3.png'),
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (ctx) => KitPage(
+                          logo: "https://i.imgur.com/MSdnfP7.png",
+                          homeKit: "Home Kit",
+                          homeKitI: "https://i.imgur.com/DSDjj8D.png",
+                          awayKit: "Away Kit",
+                          awayKitI: "https://i.imgur.com/qvVtV44.png",
+                          thirdKit: "Third Kit",
+                          thirdKitI: "https://i.imgur.com/zEnZTVp.png",
+                          gkHome: "Gk Home",
+                          gkHomeI: "https://i.imgur.com/juI9X4p.png",
+                          gkaway: "Gk Away Kit",
+                          gkawayI: "https://i.imgur.com/JEoB5As.png",
+                          gkThird: "Gk Third Kit",
+                          gkThirdI: "https://i.imgur.com/qtANUN8.png",
+                          title: "ITALY",
+                          centeredText: "ITALY Kit",
+                        ),
+                      ),
+                    );
+                  },
+                  child: Image.asset('assets/images/italy.png')),
             ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
-              child: Image.asset('assets/images/N4.png'),
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (ctx) => KitPage(
+                          logo: "https://i.imgur.com/nWjXVfF.png",
+                          homeKit: "Home Kit",
+                          homeKitI: "https://i.imgur.com/UqwTfFs.png",
+                          awayKit: "Away Kit",
+                          awayKitI: "https://i.imgur.com/6MkrO7s.png",
+                          thirdKit: "Third Kit",
+                          thirdKitI: "https://i.imgur.com/pFaErX1.png",
+                          gkHome: "Gk Home",
+                          gkHomeI: "https://i.imgur.com/CePopuO.png",
+                          gkaway: "Gk Away Kit",
+                          gkawayI: "https://i.imgur.com/ODA6C9d.png",
+                          gkThird: "Gk Third Kit",
+                          gkThirdI: "https://i.imgur.com/ardzUGa.png",
+                          title: "BRAZIL",
+                          centeredText: "BRAZIL Kit",
+                        ),
+                      ),
+                    );
+                  },
+                  child: Image.asset('assets/images/brazil.png')),
             ),
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 20, vertical: 8.0),
-              child: Image.asset('assets/images/N5.png'),
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (ctx) => KitPage(
+                          logo: "https://i.imgur.com/z6FQpNM.png",
+                          homeKit: "Home Kit",
+                          homeKitI: "https://i.imgur.com/OZVAXHM.png",
+                          awayKit: "Away Kit",
+                          awayKitI: "https://i.imgur.com/lpTpuLN.png",
+                          thirdKit: "Third Kit",
+                          thirdKitI: "loading ....",
+                          gkHome: "Gk Home",
+                          gkHomeI: "https://i.imgur.com/xhCfXza.png",
+                          gkaway: "Gk Away Kit",
+                          gkawayI: "https://i.imgur.com/mZsSv7s.png",
+                          gkThird: "Gk Third Kit",
+                          gkThirdI: "LOADING .....",
+                          title: "ARGENTINA",
+                          centeredText: "ARGENTINA Kit",
+                        ),
+                      ),
+                    );
+                  },
+                  child: Image.asset('assets/images/argentina.png')),
             ),
           ],
         ),
