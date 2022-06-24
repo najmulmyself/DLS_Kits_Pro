d5a02c424c7d060465103844e2b9547e6c992e65
najmulmyself <najmul.myself@gmail.com>
2022-06-24 15:42:58 +0600

    added other club"

--- lib/widgets/club_teams/bundesliga.dart
+++ lib/widgets/club_teams/bundesliga.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+
+import '../kit_page.dart';
 
 class Bundesliga extends StatelessWidget {
   const Bundesliga({Key? key}) : super(key: key);
@@ -17,11 +21,37 @@ class Bundesliga extends StatelessWidget {
               width: double.infinity,
               height: 300,
               child: Row(
-                children: const [
+                children:  [
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/bayern19.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              logo: "https://i.imgur.com/8Vyew7g.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.imgur.com/YrdB8ma.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.imgur.com/FrKI94W.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.imgur.com/OKUqdo6.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "https://i.imgur.com/EQJp1Kz.png",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "https://i.imgur.com/9xPx2Rr.png",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "https://i.imgur.com/aw6YqKm.png",
+                              title: "BAYERN MUNICH",
+                              centeredText: "BAYERN MUNICH Kit 19/20",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/bayern19.png'),
+                      ),
                     ),
                   ),
                   SizedBox(
@@ -29,8 +59,34 @@ class Bundesliga extends StatelessWidget {
                   ),
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/bayern20.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              logo: "https://i.imgur.com/8Vyew7g.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.imgur.com/0vBWi9k.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.imgur.com/PBxwMav.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.imgur.com/MKTwwW4.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "https://i.imgur.com/Yc0Qqqh.png",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "LOADING ....",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "LOADING ....",
+                              title: "BAYERN MUNICH",
+                              centeredText: "BAYERN MUNICH Kit 20/21",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/bayern20.png'),
+                      ),
                     ),
                   ),
                 ],
@@ -41,11 +97,37 @@ class Bundesliga extends StatelessWidget {
               width: double.infinity,
               height: 300,
               child: Row(
-                children: const [
+                children:  [
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/leipzip20.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              logo: "https://i.imgur.com/5m7bSiZ.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.imgur.com/3EydpbF.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.imgur.com/tZ62fvP.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.imgur.com/IiT3meU.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "https://i.imgur.com/yWIXgt2.png",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "LOADING ....",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "LOADING ....",
+                              title: "RB LEIPZIP",
+                              centeredText: "RB LEIPZIP Kit 20/21",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/leipzip20.png'),
+                      ),
                     ),
                   ),
                 ],
--- lib/widgets/club_teams/others_club.dart
+++ lib/widgets/club_teams/others_club.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:dls_kits_pro/widgets/kit_page.dart';
 import 'package:flutter/material.dart';
 
-import '../kit.dart';
+// import '../kit.dart';
 
 class OtherClub extends StatelessWidget {
   const OtherClub({Key? key}) : super(key: key);
@@ -29,9 +31,22 @@ class OtherClub extends StatelessWidget {
                           context,
                           MaterialPageRoute(
                             builder: (ctx) => KitPage(
-                                logo: '',
-                                title: "Fifa Ultimate Team",
-                                centeredText: "Fifa ultimate Team Kit 2022"),
+                              logo: "https://i.imgur.com/a2eecK2.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.imgur.com/YnsdXXa.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.imgur.com/ZOUteip.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.imgur.com/7laJiNf.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "loading ....",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "loading ....",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "loading ....",
+                              title: "FIFA ULTIMATE TEAM",
+                              centeredText: "FIFA ULTIMATE TEAM Kit 2020",
+                            ),
                           ),
                         );
                       },
@@ -45,29 +60,39 @@ class OtherClub extends StatelessWidget {
                   ),
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/Kerala20.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              logo: "https://i.imgur.com/ZU24Qud.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.imgur.com/L44ffLu.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.imgur.com/SFFPZL1.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.imgur.com/T2ijhtF.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "https://i.imgur.com/eS7e6k7.png",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "https://i.imgur.com/Omq4beW.png",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "LOADING .....",
+                              title: "KERALA BLASTERS",
+                              centeredText: "KERALA BLASTERS Kit 20/21",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/Kerala20.png'),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-            // Container(
-            //   margin: EdgeInsets.only(top: 5, right: 20, left: 20),
-            //   width: double.infinity,
-            //   height: 300,
-            //   child: Row(
-            //     children: const [
-            //       Expanded(
-            //         flex: 1,
-            //         child: Image(
-            //           image: AssetImage('assets/clubTeams/psg21.png'),
-            //         ),
-            //       ),
-
-            //     ],
-            //   ),
-            // ),
           ],
         ),
       ),
