a4a6546a0a16cdd767c7bdd0ac0b65f569fc6ab3
najmulmyself <najmul.myself@gmail.com>
2022-06-24 14:34:33 +0600

    added serie a team

--- lib/widgets/club_teams/serie_a.dart
+++ lib/widgets/club_teams/serie_a.dart
@@ -1,7 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
+import '../kit_page.dart';
+
 class SerieA extends StatelessWidget {
-  const SerieA({ Key? key }) : super(key: key);
+  const SerieA({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -17,11 +21,37 @@ class SerieA extends StatelessWidget {
               width: double.infinity,
               height: 300,
               child: Row(
-                children: const [
+                children: [
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/juventus19.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              logo: "https://i.imgur.com/q9XqcGl.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.imgur.com/XNIvKE4.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.imgur.com/SRKaCL4.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.imgur.com/asaGAVs.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "https://i.imgur.com/Hx4HZsY.png",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "https://i.imgur.com/4IUyLf7.png",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "https://i.imgur.com/Zex8mnh.png",
+                              title: "JUVENTUS",
+                              centeredText: "JUVENTUS Kit 19/20",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/juventus19.png'),
+                      ),
                     ),
                   ),
                   SizedBox(
@@ -29,8 +59,34 @@ class SerieA extends StatelessWidget {
                   ),
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/juventus20.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              logo: "https://i.imgur.com/q9XqcGl.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.imgur.com/YbocGVb.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.imgur.com/gogD54L.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.imgur.com/4rltVJE.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "https://i.imgur.com/QzTXISe.png",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "https://i.imgur.com/sgOOcfA.png",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "https://i.imgur.com/L0jVOjc.png",
+                              title: "JUVENTUS",
+                              centeredText: "JUVENTUS Kit 20/21",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/juventus20.png'),
+                      ),
                     ),
                   ),
                 ],
@@ -41,11 +97,37 @@ class SerieA extends StatelessWidget {
               width: double.infinity,
               height: 300,
               child: Row(
-                children: const [
+                children: [
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/juventus21.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              logo: "https://i.imgur.com/q9XqcGl.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.ibb.co/hX1jHYb/p1.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.ibb.co/6vPVPQ7/p2.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "https://i.ibb.co/mTDHtN1/p3.png",
+                              gkHome: "Gk Home",
+                              gkHomeI: "https://i.ibb.co/1mJJQNB/p4.png",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "loading....",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "loading ....",
+                              title: "JUVENTUS",
+                              centeredText: "JUVENTUS Kit 21/22",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/juventus21.png'),
+                      ),
                     ),
                   ),
                   SizedBox(
@@ -53,8 +135,34 @@ class SerieA extends StatelessWidget {
                   ),
                   Expanded(
                     flex: 1,
-                    child: Image(
-                      image: AssetImage('assets/clubTeams/intr_milan21.png'),
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (ctx) => KitPage(
+                              logo: "https://i.imgur.com/Cwb24yv.png",
+                              homeKit: "Home Kit",
+                              homeKitI: "https://i.imgur.com/33cRigT.png",
+                              awayKit: "Away Kit",
+                              awayKitI: "https://i.imgur.com/33cRigT.png",
+                              thirdKit: "Third Kit",
+                              thirdKitI: "loading....",
+                              gkHome: "Gk Home",
+                              gkHomeI: "loading....",
+                              gkaway: "Gk Away Kit",
+                              gkawayI: "loading....",
+                              gkThird: "Gk Third Kit",
+                              gkThirdI: "loading....",
+                              title: "INTER MILAN",
+                              centeredText: "INTER MILAN Kit 21/22",
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image(
+                        image: AssetImage('assets/clubTeams/intr_milan21.png'),
+                      ),
                     ),
                   ),
                 ],
