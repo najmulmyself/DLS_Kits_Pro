bbf7d599fbcd89372f0bb0314fb5ed5793e61094
najmulmyself <najmul.myself@gmail.com>
2022-10-01 23:38:27 +0600

    added newUpdate function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:dls_kits_pro/widgets/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:new_version/new_version.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +11,38 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    checkVersion();
+  }
+
+  Future<VersionStatus>? checkVersion() async {
+    final newVersion = await NewVersion(
+      androidId: 'com.snapchat.android',
+      // iOSId: 'com.dlskitspro',
+    );
+    final VersionStatus? status = await newVersion.getVersionStatus();
+    print(status);
+    newVersion.showUpdateDialog(
+      context: context,
+      versionStatus: status!,
+      dialogTitle: "Update Available",
+      dismissButtonText: "Skip",
+      dismissAction: () {
+        SystemNavigator.pop();
+      },
+      dialogText: "Please update the app to continue using it.",
+    );
+    throw UnimplementedError();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
