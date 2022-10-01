c1f722f223600cb244f4e230dd1747ecd36d472b
najmulmyself <najmul.myself@gmail.com>
2022-10-02 02:17:20 +0600

    added upgrader not yet working

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 // import 'package:new_version/new_version.dart';
 import 'package:new_version_plus/new_version_plus.dart';
+import 'package:upgrader/upgrader.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,14 +25,16 @@ class _MyAppState extends State<MyApp> {
     checkVersion();
   }
 
-  Future<VersionStatus>? checkVersion() async {
-    final newVersion = NewVersionPlus(
+  Future checkVersion() async {
+    final newVersionPlus = NewVersionPlus(
       androidId: 'com.snapchat.android',
       // iOSId: 'com.dlskitspro',
     );
-    final VersionStatus? status = await newVersion.getVersionStatus();
-    print(status);
-    newVersion.showUpdateDialog(
+    final status = await newVersionPlus.getVersionStatus();
+    print(status?.appStoreLink);
+    print(status?.localVersion);
+    print(status?.storeVersion);
+    newVersionPlus.showUpdateDialog(
       context: context,
       versionStatus: status!,
       dialogTitle: "Update Available",
@@ -54,7 +57,9 @@ class _MyAppState extends State<MyApp> {
       ),
       debugShowCheckedModeBanner: false,
       title: 'DLS Kits Pro',
-      home: HomeScreen(),
+      home: UpgradeAlert(
+          upgrader: Upgrader(dialogStyle: UpgradeDialogStyle.cupertino),
+          child: HomeScreen()),
     );
   }
 }
